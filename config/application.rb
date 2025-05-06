84d349b590ab94657b6c7dc5a95283b4ae513d34
nguyenngoctung75 <tungit1huster@gmail.com>
2025-05-06 09:15:38 +0700

    'chapter6'

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -17,5 +18,5 @@ module SampleApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-  end
+  end  
 end
