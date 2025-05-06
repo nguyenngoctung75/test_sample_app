93f82847cf7dbe9621049c14d4f11c7ec8c7d3d9
nguyenngoctung75 <tungit1huster@gmail.com>
2025-05-06 17:54:15 +0700

    'c7'

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -5,6 +5,7 @@ class ApplicationHelperTest < ActionView::TestCase
   #   assert_equal full_title, "Ruby on Rails Tutorial Sample App"
   #   assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
   # end
+
   test "full_title helper" do
     assert_equal full_title, "Ruby on Rails Tutorial Sample App"
     assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
